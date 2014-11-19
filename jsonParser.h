95d40c77a94acf18afdfa6bfd32a907165d5261a
Joshua M. Sacks <jmsacks@gmail.com>
2014-11-19 11:37:55 -0800

    created json parser

---
+++ jsonParser.h
@@ -0,0 +1,15 @@
+//
+//  jsonParser.h
+//  GroupProject2nd
+//
+//  Created by Joshua M. Sacks on 11/19/14.
+//  Copyright (c) 2014 Tuan Vu. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface jsonParser : NSObject
+
++ (NSArray *) parseJSONIntoListArray: (NSData *) jsonData;
+
+@end
